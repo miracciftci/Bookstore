09469db1ba601172683fa987b851ac0ebc7ff232
Mirac <miracciftci.cs@gmail.com>
2023-11-02 14:26:06 +0300

    commit

--- bookstore/src/main/java/com/shop/bookstore/service/rules/BookCommentsBusinnesRules.java
+++ bookstore/src/main/java/com/shop/bookstore/service/rules/BookCommentsBusinnesRules.java
@@ -1,6 +1,8 @@
 package com.shop.bookstore.service.rules;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.shop.bookstore.core.utilities.exceptions.BusinessException;
 import com.shop.bookstore.entities.BookComments;
@@ -13,6 +15,8 @@ import lombok.AllArgsConstructor;
 public class BookCommentsBusinnesRules {
 	BookCommentsRepository bookCommentsRepository;
 	
+	//@Value("${bookstore.constraint.bookcomment.checkCommentAgain}")
+	//String message;
 	
 	public void checkCommentId(Long id) {
 		BookComments bookComments = bookCommentsRepository.findById(id).orElseThrow(()-> new BusinessException("Comment id is not found"));
@@ -21,10 +25,10 @@ public class BookCommentsBusinnesRules {
 	public void checkCommentAgain(Long accountId, Long bookId) {
 		List<BookComments> gets = bookCommentsRepository.findAll();
 		
-	
+		
 		for(BookComments bookComments : gets) {
 			if(accountId == bookComments.getAccount().getId() && bookId == bookComments.getBook().getId()) {
-				throw new BusinessException("2 comments cannot be shared on the same book from 1 account.");
+				throw new BusinessException("2 comments cannot be shared on the same book from 1 account");
 			}
 		}
 		
